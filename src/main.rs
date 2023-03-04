30bbb101754ffbd600b25c7bdac88fdb03d82403
nayan <nayansharma1010@gmail.com>
2023-03-04 11:56:50 +0530

    feat: added tcp connection with read and write to socket

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+use tokio::{net::TcpListener, io::{ AsyncWriteExt,BufReader, AsyncBufReadExt}};
+
 #[tokio::main]
 async fn main() {
-    println!("lets go chat");
-}
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
+    let (mut socket,_addr) = listener.accept().await.unwrap();
+
+
+    let (read ,mut write ) = socket.split();
+
+    let mut reader = BufReader::new(read);
+    let mut line: String = String::new();
+
+    loop {
+        let bytes_read: usize = reader.read_line(&mut line).await.unwrap();
+
+        if bytes_read == 0 {
+            break;
+        } 
+        
+        write.write_all(line.as_bytes()).await.unwrap() ;
+    }
+}   
