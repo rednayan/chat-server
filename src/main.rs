14ef2ae207cac2976290c836e960a1fcb1a55204
nayan <nayansharma1010@gmail.com>
2023-03-04 12:44:43 +0530

    feat:multiple connections

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,25 @@ use tokio::{net::TcpListener, io::{ AsyncWriteExt,BufReader, AsyncBufReadExt}};
 #[tokio::main]
 async fn main() {
     let listener: TcpListener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
-    let (mut socket,_addr) = listener.accept().await.unwrap();
-
-
-    let (read ,mut write ) = socket.split();
-
-    let mut reader = BufReader::new(read);
-    let mut line: String = String::new();
 
     loop {
-        let bytes_read: usize = reader.read_line(&mut line).await.unwrap();
+        let (mut socket,_addr) = listener.accept().await.unwrap();
 
-        if bytes_read == 0 {
-            break;
-        } 
-        
-        write.write_all(line.as_bytes()).await.unwrap() ;
+        tokio::spawn(async move {           
+                let (read ,mut write ) = socket.split();
+            
+                let mut reader = BufReader::new(read);
+                let mut line: String = String::new();
+            
+                loop {
+                    let bytes_read: usize = reader.read_line(&mut line).await.unwrap();
+            
+                    if bytes_read == 0 {
+                        break;
+                    } 
+                    write.write_all(line.as_bytes()).await.unwrap() ;
+                    line.clear();
+                }
+        });
     }
 }   
