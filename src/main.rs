5fbef4116c4fb3715f7279073c7e9e0c6c4a0bb7
nayan <nayansharma1010@gmail.com>
2023-03-04 13:14:52 +0530

    feat:added broadcast channel

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,39 @@
-use tokio::{net::TcpListener, io::{ AsyncWriteExt,BufReader, AsyncBufReadExt}};
+use tokio::{net::TcpListener, io::{ AsyncWriteExt,BufReader, AsyncBufReadExt}, sync::broadcast};
 
 #[tokio::main]
 async fn main() {
     let listener: TcpListener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
+    let (tx, rx) = broadcast::channel(10);
 
     loop {
-        let (mut socket,_addr) = listener.accept().await.unwrap();
+        let (mut socket,addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move {           
                 let (read ,mut write ) = socket.split();
             
                 let mut reader = BufReader::new(read);
                 let mut line: String = String::new();
-            
+
                 loop {
-                    let bytes_read: usize = reader.read_line(&mut line).await.unwrap();
-            
-                    if bytes_read == 0 {
-                        break;
-                    } 
-                    write.write_all(line.as_bytes()).await.unwrap() ;
-                    line.clear();
-                }
+                    tokio::select! {
+                        result = reader.read_line(&mut line) => {
+                            if result.unwrap() == 0 {
+                                break;
+                            }
+                            tx.send((line.clone(),addr)).unwrap();
+                            line.clear();
+                        }
+                        result= rx.recv() => {
+                            let (msg,other_addr) = result.unwrap();
+                            
+                            if addr != other_addr {
+                                write.write_all(msg.as_bytes()).await.unwrap();
+                            }
+                        }
+                    }
+                }  
         });
     }
 }   
